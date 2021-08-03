be0ea2ba01480272ed75164b820c5a9d32da6735
mgavillo <mgavillo@student.42.fr>
2021-08-03 14:51:03 +0200

    cleaned describe

--- describe.py
+++ describe.py
@@ -3,86 +3,80 @@ import numpy as np
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
-
+import sys
 import seaborn as sns
 
+class Describe():
 
+    def __init__(self, data):
+        self.data = data
+        self.dataNum = data.iloc[:,6:]
+        self.dataSorted = self.dataNum.transform(np.sort)
+        self.N = len(self.dataNum.columns)
+        self.count = [0 for x in range(self.N)]
+        self.mean = self.count[:]
 
-dataNum = data.iloc[:,6:]
-N = len(dataNum.columns)
-min = [0 for x in range(N)]
-max = min[:]
-count = min[:]
-mean = min[:]
-q1 = min[:]
-q2 = min[:]
-q3 = min[:]
-std = min[:]
-
-
-        data = pd.read_csv(sys.argv[1])
-
-    def read_and_clean():
-
-    def calc_count_mean():
+    def calc_count_mean(self):
         '''
         for each feature, calulate count without blank input
         then calculate average
         '''
-        count = min[:]
-        mean = min[:]
-        for index, column in enumerate(dataNum.columns):
-            count[index] = dataNum[column].notnull().sum()
-            for d in dataNum[column]:
+        for index, column in enumerate(self.dataNum.columns):
+            self.count[index] = self.dataNum[column].notnull().sum()
+            for d in self.dataNum[column]:
                 if not math.isnan(d):
-                    mean[index] += d
-            mean[index] /= count[index]        
+                    self.mean[index] += d
+            self.mean[index] /= self.count[index]        
 
-    def calc_std():
+    def calc_std(self):
         '''
         for each feature, calculate standart deviation, a measure of
         the spread of a distribution
 
         sdt = sqrt(mean(abs(x - x.mean())**2))
         '''
-        std = [0 for x in range(N)]
-        for index, column in enumerate(dataNum):
+        std = [0 for x in range(self.N)]
+        for index, column in enumerate(self.dataNum):
             deviations = []
-            for x in dataNum[column]:
+            for x in self.dataNum[column]:
                 if not math.isnan(x):
-                    deviations.append((x - mean[index]) ** 2)
-            variance = sum(deviations) / count[index]
-            std[index] = math.sqrt(variance[index])
-        return(std)
+                    deviations.append((x - self.mean[index]) ** 2)
+            variance = sum(deviations) / self.count[index]
+            std[index] = math.sqrt(variance)
+        return std
 
-data_process.calc_count_mean()
-data_process.calc_std()
-percentile = dataNum
+    def calc_percentile(self, perc, j, index):
+        x = perc * self.count[index]
+        i = int(x)
+        ret = self.dataSorted[j][i] + ((x - int(x)) * (self.dataSorted[j][i + 1] - self.dataSorted[j][i]))
+        return ret
 
-percentile = percentile.transform(np.sort)
-for index, column in enumerate(percentile):
-    i = 0.75 * count[index]
-    i2 = 0.5 * count[index]
-    # print(math.floor(0.25 * count[index]))
-    # print(math.ceil(0.25 * count[index]))
-    x = 0.25 * (count[index] + 1)
-    i = int(x)
-    q1[index] = percentile[column][i]
-    if not x.is_integer():
-        print(x - int(x))
-        q1[index] += (x - int(x)) * (percentile[column][i + 1] -  percentile[column][i])
-    q2[index] = percentile[column][math.floor(0.5 * count[index]) - 1]
-    q3[index] = percentile[column][math.floor(0.75 * count[index]) - 1]
-    min[index] = percentile[column][0]
-    max[index] = percentile[column][count[index] - 1]
+    def calc_percentiles(self):
+        self.perc = self.dataNum
+        self.perc = self.perc.transform(np.sort)
 
-data_process(sys.argv[1])
-print()
-print(percentile)
-print(dataNum.describe())
+        min = [0 for x in range(self.N)]
+        max = min[:]
+        q1 = min[:]
+        q2 = min[:]
+        q3 = min[:]
+        for index, column in enumerate(self.dataSorted):
+            q1[index] = self.calc_percentile(0.25, column, index)
+            q2[index] = self.calc_percentile(0.5, column, index)
+            q3[index] = self.calc_percentile(0.75, column, index)
+            min[index] = self.dataSorted[column][0]
+            max[index] = self.dataSorted[column][self.count[index] - 1]
+        return q1, q2, q3, min, max
 
-d = [count, mean, std, min, q1, q2, q3, max]
-df = pd.DataFrame(d, columns = dataNum.columns, index=["count", "mean", "std", "min", "25%", "50%", "75%", "max"])
-print(df)
+    def show(self):
+        self.calc_count_mean()
+        std = self.calc_std()
+        q1, q2, q3, min, max = self.calc_percentiles()
+        d = [self.count, self.mean, std, min, q1, q2, q3, max]
+        df = pd.DataFrame(d, columns = self.dataNum.columns, index=["count", "mean", "std", "min", "25%", "50%", "75%", "max"])
+        print(df)
 
-    
+if __name__ == "__main__":
+    data = pd.read_csv(sys.argv[1])
+    describe = Describe(data)
+    describe.show()
---
+++ logreg_train.py
@@ -0,0 +1,3 @@
+from describe import Describe
+
+
